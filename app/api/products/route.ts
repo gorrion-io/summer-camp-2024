9d7c106be784cf6acb24e74e3ba81ed0044f8526
Fabi0o <marfab32@gmail.com>
2024-06-22 16:26:00 +0200

    feat: creat endpoint for fetching paginated products

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,9 @@
-/**
- * TODO: Prepare an endpoint to return a list of products
- * The endpoint should return a pagination of 10 products per page
- * The endpoint should accept a query parameter "page" to return the corresponding page
- */
+import { fetchProducts } from "@/lib/products";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-    return Response.json([]);
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const page = searchParams.get("page");
+
+  return Response.json(fetchProducts(Number(page)));
 }
