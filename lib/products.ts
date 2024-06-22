b2ef1265f856b4d3e6e69bc5c412b0faf1330485
Deniorrr <kujonek95@gmail.com>
2024-06-22 17:16:34 +0200

    fetching from Json works

--- lib/products.ts
+++ lib/products.ts
@@ -1,3 +1,6 @@
+import { readFile } from "fs/promises";
+import fs from "fs";
+
 export type Product = {
   id: string;
   name: string;
@@ -7,7 +10,20 @@ export type Product = {
   isAlcohol: boolean;
 };
 
-export function fetchProducts(page: number): Product[] {
-  // todo
+const fetchJsonProducts = async (): Promise<Product[]> => {
+  const data = await readFile("products.json", "utf-8");
+  const products: Product[] = JSON.parse(data);
+  return products;
+};
+
+const fetchCSVProducts = async (): Promise<Product[]> => {
   return [];
+};
+
+export async function fetchProducts(page: number): Promise<Product[]> {
+  const products = (await fetchJsonProducts()).sort((a, b) =>
+    a.id.localeCompare(b.id)
+  );
+  const startIndex = (page - 1) * 10; // Calculate the starting index
+  return products.slice(startIndex, startIndex + 10); // Return 10 products starting from startIndex
 }
