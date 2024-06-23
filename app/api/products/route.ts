6c06d30c06c8e73d258957e87d9df4be1fb70203
Fabi0o <marfab32@gmail.com>
2024-06-23 14:17:02 +0200

    fix: change endpoint to accept page number based from 1 for better ux and consistancy with pagination metadata

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,6 +4,7 @@ import { NextRequest } from "next/server";
 export function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const page = searchParams.get("page");
+  const products = fetchProducts(Number(page) - 1);
 
-  return Response.json(fetchProducts(Number(page)));
+  return Response.json(products);
 }
