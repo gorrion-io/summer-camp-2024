97657707949f1d325839766920c0c5c343996f12
revinto <jakub.gornikk@gmail.com>
2024-06-23 23:55:52 +0200

    feat: add function for paginating products list, api endpoint

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,25 @@
+import { fetchProducts } from "@/lib/products";
+import { NextRequest, NextResponse } from "next/server";
+import { PaginatedResponse } from "@/app/types/paginatedResponseType";
+
 /**
  * TODO: Prepare an endpoint to return a list of products
  * The endpoint should return a pagination of 10 products per page
  * The endpoint should accept a query parameter "page" to return the corresponding page
  */
 
-export async function GET() {
-    return Response.json([]);
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const pageNumber = parseInt(searchParams.get("page") as string);
+
+  const { paginatedProductsList, totalNumberOfPages, allProductsCount } =
+    await fetchProducts(pageNumber);
+
+  const response: PaginatedResponse = {
+    paginatedProductsList,
+    totalNumberOfPages,
+    allProductsCount,
+  };
+
+  return NextResponse.json(response, { status: 200 });
 }
--- lib/products.ts
+++ lib/products.ts
@@ -1,6 +1,29 @@
-import { Product } from "@/app/types/productTypes";
+import mergeCsvAndJsonProducts from "./utils/mergeCsvAndJsonFiles";
+import { PaginatedResponse } from "@/app/types/paginatedResponseType";
+import { PAGE_SIZE } from "@/app/shared/consts";
 
-export function fetchProducts(page: number): Product[] {
-  // todo
-  return [];
+export async function fetchProducts(page: number): Promise<PaginatedResponse> {
+  const productsList = await mergeCsvAndJsonProducts();
+
+  const filteredProducts = productsList.filter((product) => !product.isAlcohol);
+  const sortedFilteredProducts = filteredProducts.sort((a, b) =>
+    a.id.localeCompare(b.id)
+  );
+
+  const startIndex = (page - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+
+  const paginatedProductsList = sortedFilteredProducts.slice(
+    startIndex,
+    endIndex
+  );
+  const totalNumberOfPages = Math.ceil(
+    sortedFilteredProducts.length / PAGE_SIZE
+  );
+
+  return {
+    paginatedProductsList,
+    totalNumberOfPages,
+    allProductsCount: sortedFilteredProducts.length,
+  };
 }
