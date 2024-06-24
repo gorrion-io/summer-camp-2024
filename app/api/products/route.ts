8930f5d2ed6818441883268282dfaabcbfcbd95f
jtab <jakuq12@gmail.com>
2024-06-24 18:55:47 +0200

    endpoint to fetching list of products from concrete page and amount of all products

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,20 @@
-/**
- * TODO: Prepare an endpoint to return a list of products
- * The endpoint should return a pagination of 10 products per page
- * The endpoint should accept a query parameter "page" to return the corresponding page
- */
 
-export async function GET() {
-    return Response.json([]);
+import { NextRequest, NextResponse } from 'next/server';
+import { fetchProducts, getCountProducts } from '@/lib/products';
+//endpoint returning products from concrete page and amount of all products using fetchproducts and getcountproducts functions from lib
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const page = parseInt(searchParams.get('page') || '0', 10);
+    
+    
+    const products = await fetchProducts(page);
+    const total = await getCountProducts();
+
+    return NextResponse.json({ products, total });
+  } catch (error) {
+    
+    console.error(error);
+    return NextResponse.json({ error: 'Failed to fetch products' }, { status: 500 });
+  }
 }
