9f1b862367494b2bf4d94ef2097e4d6495490629
akolt19d <a.kolt.19d@gmail.com>
2024-06-24 18:31:16 +0200

    feat(app): added parsing files and returning data

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,5 @@
-/**
- * TODO: Prepare an endpoint to return a list of products
- * The endpoint should return a pagination of 10 products per page
- * The endpoint should accept a query parameter "page" to return the corresponding page
- */
+import { fetchProducts } from "@/lib/products";
 
 export async function GET() {
-    return Response.json([]);
+    return new Response(JSON.stringify(fetchProducts(1)));
 }
--- lib/products.ts
+++ lib/products.ts
@@ -1,3 +1,8 @@
+import * as fs from "fs";
+
+// CONSTS
+const PAGE_SIZE = 20;
+
 export type Product = {
     id: string;
     name: string;
@@ -7,7 +12,31 @@ export type Product = {
     isAlcohol: boolean;
 };
 
+function parseCsvData(): Product[] {
+    const data = fs.readFileSync("products.csv", "utf8");
+    const lines = data.split("\n").slice(1);
+    let products: Product[] = [];
+    lines.forEach((line) => {
+        const [id, name, price, currency, quantity, isAlcohol] = line.split(",");
+        products.push({
+            id,
+            name,
+            price: parseFloat(price),
+            currency,
+            quantity: parseInt(quantity),
+            isAlcohol: isAlcohol === "true",
+        });
+    })
+
+    return products;
+}
+
+function parseJsonData(): Product[] {
+    const data = fs.readFileSync("products.json", "utf8");
+    return JSON.parse(data);
+}
+
 export function fetchProducts(page: number): Product[] {
-    // todo
-    return [];
+    const data = [...parseCsvData(), ...parseJsonData()].filter((product) => !product.isAlcohol).sort((a, b) => a.id.localeCompare(b.id));
+    return data.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE);
 }
