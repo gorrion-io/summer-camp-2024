b4af6d7dff4d013a314fa4f8f9eedbc0d1552c1e
Vertori <115083330+Vertori@users.noreply.github.com>
2024-06-24 13:12:03 +0200

    Modified tests

--- lib/products.test.ts
+++ lib/products.test.ts
@@ -1,19 +1,22 @@
-import { expect, test } from "vitest"
-import { fetchProducts } from "./products"
+import { expect, test } from "vitest";
+import { getProducts } from "./products";
 
-test('alcohol is excluded', () => {
-    const products = fetchProducts(0);
-    expect(products.some(product => product.isAlcohol)).toBe(false);
+test("alcohol is excluded", async () => {
+  const response = await getProducts(1);
+  const products = response.paginatedProducts;
+  expect(products.every((product) => !product.isAlcohol)).toBe(true);
 });
 
-test('page has 10 items', () => {
-    const products = fetchProducts(0);
-    expect(products).toHaveLength(10);
+test("page has 10 items", async () => {
+  const response = await getProducts(1);
+  const products = response.paginatedProducts;
+  expect(products).toHaveLength(10);
 });
 
-test('products are sorted', () => {
-    const products = fetchProducts(0);
-    const ids = products.map(product => product.id);
-    const sortedIds = [...ids].sort((a, b) => a.localeCompare(b));
-    expect(ids).toEqual(sortedIds);
+test("products are sorted", async () => {
+  const response = await getProducts(1);
+  const products = response.paginatedProducts;
+  const ids = products.map((product) => product.id);
+  const sortedIds = [...ids].sort((a, b) => a.localeCompare(b));
+  expect(ids).toEqual(sortedIds);
 });
