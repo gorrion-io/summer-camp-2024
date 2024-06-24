56dc62a06b8e23741ba06f464728d47ac1551e00
Vertori <115083330+Vertori@users.noreply.github.com>
2024-06-24 11:18:03 +0200

    Created functions to load data

--- lib/products.ts
+++ lib/products.ts
@@ -1,13 +1,41 @@
-export type Product = {
-    id: string;
-    name: string;
-    price: number;
-    currency: string;
-    quantity: number;
-    isAlcohol: boolean;
+import fs from "fs/promises";
+import { parse } from "csv-parse/sync";
+import { CsvProduct, Product } from "@/types";
+
+export const loadJsonProducts = async (path: string): Promise<Product[]> => {
+  try {
+    const jsonData = await fs.readFile(path, "utf8");
+    return JSON.parse(jsonData);
+  } catch (err) {
+    throw new Error(
+      err instanceof Error
+        ? err.message
+        : "Failed to load JSON file, an unknown error happened."
+    );
+  }
 };
 
-export function fetchProducts(page: number): Product[] {
-    // todo
-    return [];
-}
+export const loadCsvProducts = async (path: string): Promise<Product[]> => {
+  try {
+    const csvData = await fs.readFile(path, "utf-8");
+    const records = parse(csvData, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+
+    return records.map((record: CsvProduct) => ({
+      id: record.Id,
+      name: record.Name,
+      price: parseFloat(record.Price),
+      currency: record.Currency,
+      quantity: parseInt(record.Quantity, 10),
+      isAlcohol: record.IsAlcohol === "1",
+    }));
+  } catch (err) {
+    throw new Error(
+      err instanceof Error
+        ? err.message
+        : "Failed to load CSV file, an unknown error happened."
+    );
+  }
+};
