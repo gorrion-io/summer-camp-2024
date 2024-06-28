84403d26f28e48fd74fa1d4a0642924e73fd0cee
Wapa <72733062+mtszmaron@users.noreply.github.com>
2024-06-28 21:55:03 +0200

    Finished task
    - Added parsing data from files
    - Added endpoint in API
    - Added pagination on page
    - Changed test to match new type

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,11 @@
-/**
- * TODO: Prepare an endpoint to return a list of products
- * The endpoint should return a pagination of 10 products per page
- * The endpoint should accept a query parameter "page" to return the corresponding page
- */
+import { fetchProducts } from "@/lib/products";
+import { ProductResponse } from "@/lib/products";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-    return Response.json([]);
+export async function GET(request: NextRequest) {
+  const page = request.nextUrl.searchParams.get("page");
+  const productResponse: ProductResponse = fetchProducts(
+    page ? parseInt(page) : 1
+  );
+  return Response.json(productResponse);
 }
